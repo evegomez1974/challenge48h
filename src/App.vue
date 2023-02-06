e0559543ea8fe848ec8fbad2bd9164e6b6604499
Pikselle <Maureen.metge@gmail.com>
2023-02-06 12:46:55 +0100

    router qui fonctionne

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Accueil msg="Welcome to Your Vue.js App"/>
+  <router-link to="/Accueil">Accueil</router-link>
+  <router-link to="/BestPage">Oui</router-link>
   <router-view />
 </template>
 
 <script>
-import Accueil from './components/Accueil.vue'
 
 export default {
   name: 'App',
   components: {
-    Accueil
+  
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,26 +1,9 @@
 import { createApp } from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
-import Accueil from './components/Accueil.vue';
-import PageTestMaGueule from './components/PageTestMaGueule.vue';
+import App from './App.vue';
 
+import router from './router.js';
 
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
-        {
-            path: "/Accueil",
-            name: "Accueil",
-            component: Accueil
-        },
-        {
-            path: "/BestPage",
-            name: "BestPage",
-            component: PageTestMaGueule
-        }
-    ]
-})
-
-createApp(Accueil)
+createApp(App)
     .use(router)
     .mount('#app')
 
